46fb6debef44b49913c8694815d5b6f427a6a305
amaya-samirah <amaya.s.26292@gmail.com>
2025-03-07 15:00:30 -0500

    Updated HomeController

--- 2048GUI/src/main/java/controllers/HomeController.java
+++ 2048GUI/src/main/java/controllers/HomeController.java
@@ -4,20 +4,33 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import model.Game;
+import model.UserList;
 
 public class HomeController {
 
     @FXML
-    private Button login;
+    private Button button_login;
 
     @FXML
-    private TextField password;
+    private TextField txt_password;
 
     @FXML
-    private TextField userName;
+    private TextField txt_userName;
 
     @FXML
     void loginClicked(MouseEvent event) {
+        String userName = txt_userName.getText();
+        String password = txt_password.getText();
+
+        UserList users = UserList.getInstance();
+        Game game = new Game(users);
+
+        boolean loginSuccess = game.logIn(userName, password);
+        if (!loginSuccess) {
+            System.out.println("Login Failed");
+        }
+        System.out.println("Login Success");
 
     }
 
--- 2048GUI/src/main/java/game/gui/App.java
+++ 2048GUI/src/main/java/game/gui/App.java
@@ -12,13 +12,9 @@ public class App extends Application {
     private static Scene scene;
 
     public void start(Stage stage) throws IOException {
-        System.out.println("Reached start method");
         scene = new Scene(loadFXML("home"), 389, 473);
-        System.out.println("Reached new scene creation");
         stage.setScene(scene);
-        System.out.println("Reached stage set scene");
         stage.show();
-        System.out.println("Reached stage show");
     }
 
     public static void setRoot(String fxml) throws IOException {
